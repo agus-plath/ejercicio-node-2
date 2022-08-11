7d9dbdcc9ce9eb423fc3990a85fdb0796449e900
agus-plath <agustinna.roldan@gmail.com>
2022-08-10 23:51:59 -0300

    ejercicio 2 de nodejs

---
+++ ejercicio-2.js
@@ -0,0 +1,11 @@
+let lista = ["Agustina", 21, true, new Date(2001, 2, 24)];
+console.log(lista);
+
+lista.push({libro: {
+    titulo: "Mi año de descanso y relajación",
+    autor: "Ottessa Moshfegh",
+    fecha: new Date(2019, 06, 01),
+    url: "https://www.penguinlibros.com/ar/literatura-contemporanea/164684-libro-mi-ano-de-descanso-y-relajacion-9789877385977",
+} });
+
+console.log(lista);
